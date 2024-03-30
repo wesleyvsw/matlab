7864e7a243607d6e9dcdfad6aeb80d8f26e4b61d
wesleyvsw <64928214+wesleyvsw@users.noreply.github.com>
2024-03-30 13:26:20 -0300

    Add files via upload

---
+++ lista1.m
@@ -0,0 +1,20 @@
+% Exercício 1%
+Z = [1-2j 1+2j 2-j;2 1+2j 3j];
+
+abs(Z)
+angle(Z)
+real(Z)
+imag(Z)
+conj(Z)
+Z'
+exp(Z)
+log(Z)
+log10(Z)
+
+
+%Eercicio 2%
+
+x = [1 10 7 0.10]
+exp(x)
+log(x)
+log10(x)
